dc33768a68da75203dd639063a38bffad2048609
Terry Rozmus <terry.rozmus@gmail.com>
2018-08-29 21:25:22 +1200

    Commit five

--- bouncing-earth/a1_17.js
+++ bouncing-earth/a1_17.js
@@ -1,43 +1,35 @@
-/*
-My Changes and Additions
-(further comments are in the lines quoted below)
+x`x`/*
+My methods for constructing lights and doing shadow calculations
 
-1. Lines 76 to 83: Added some lines which boot-strap a bug
-   which is fills up the console with warning messages
-   (at least in browsers on my Windows 10 installation).
-2. Lines 89 to 91: Commented out a line which changes a variable
-   which has already been given a value in line 47 and also
-   fills the console with errors in my installation.
-3. Lines 113 to 119: Loaded a tablecloth texture for the table.
-4. Lines 181 to 206: Hard-coded the vertex position, normal
-   vertex texture coordinate data at the four corners of a 
-   plane.
-5. Lines 357 to 374: Under the moonAngle and earthAngle 
-   variable declarations, I added further variable declarations
-   for Earth, moon and zoom positions.
-6. From line 376: drawScene():
-   a. Lines 386, 411 and 438: modified the use of the modelview 
-      matrix (mvMatrix) translations so that they now encode
-      the varying 3d positions and 'zoom' factors for the 
-	  table, moon and earth separately.
-   b. Line 415: Changed rotation axis of the moon to x (from y)
-      and reversed the rotation direction (I wanted the moon
-      to roll up the table instead of across it).
-7. Lines 461 to 665: Set rotation increments and calculated
-   distance increments for the moon and earth in the scene
-   respectively based on their radius and rotation
-   increment magnitude.
-8. From line 467: render():
-   a. Set Earth and Moon angles and positions using the 
-      rotation and distance increments described above in 7.
-   b. Made the earth and moon perform free-fall motions when 
-      they are off the edge of the table.
-   c. Set up a zoom over time motion which is eased at the 
-      start and end of the motion using a Gaussian function 
-	  to make the motion look a little more natural.
-9. From line 551: functions applied to the zoom button
-   to control which zoom-factor is currently active and which
-   zoom button is currently visible. 
+1. The lights were constructed basically in the same manner as described in the book.
+2. To generate shadows I exploited the fact that the main scene objects are
+   spheres. I tested whether the light from a source should be used to illuminate
+   a pixel by working out whether a vector to the light passes through a circle
+   drawn around the central point of the sphere with the same radius as the sphere
+   and perpendicular to the vector. If the vector is inside the circle, then the
+   the light will not be added to the surface and so the area becomes shadow.
+
+Ideas for other things I could try or improve:
+
+1. One thing I thought about doing in addition to the standard set of light is 
+   making a long tube light (like an office fluorescent light, but time was 
+   pressing. The idea is to model the the light as a line segment, but to consider 
+   the rays striking each point on surfaces as originating from whichever point 
+   along the light is nearest to the surface point. Alternatively a random point 
+   along the light could be considered the origin point of the light ray for each 
+   incident surface point, but I'm not sure how well this would work with animation.
+2. Instead of calculating whether a point is in shadow by checking if the vector
+   from the surface point to the light source passes through either of the spheres
+   (Earth and Moon), I could have generalised this calculation by seeing if the 
+   same vector passes through any of the triangles that make up the sphere. This may
+   technically involve more calculations, but these could be reduced by using a low-
+   polygon stand-in model for calculating shadows, or by filter the object vertices
+   in some way so that only triangles that the ray passes near to are checked. Only
+   doing the calculations if the ray passes through a bounding box or sphere may also
+   help to lower any overhead created by this approach.
+3. Use functions or classes to calculate and model lights, shadows and positions of 
+   objects. Now that I understand things better, using a more structured program
+   would allow me to simplify my code.
 */
 
 var gl;
@@ -61,7 +53,8 @@ window.onload = function init() {
 
         gl.clearColor(0.0, 0.0, 0.0, 1.0);
         gl.enable(gl.DEPTH_TEST);
-
+        //gl.enable(gl.CULL_FACE);
+        gl.cullFace(gl.BACK);
         render();
 }
 
@@ -73,20 +66,25 @@ function defineAttributes() {
 	gl.enableVertexAttribArray(shaderProgram.textureCoordAttribute);
 
 	shaderProgram.vertexNormalAttribute = gl.getAttribLocation(shaderProgram, "aVertexNormal");
-	/* The following lines Eliminate an unknown error in the 
-	provided code (at least it appears in Windows 10 in all 
-	browsers I used). The error does not appear to be fatal 
-	to the operation of the program, but it does fill up 
-	the console with warning messages. (LINES COMMENTED OUT IN 
+	/* The following lines Eliminate an unknown error in the
+	provided code (at least it appears in Windows 10 in all
+	browsers I used). The error does not appear to be fatal
+	to the operation of the program, but it does fill up
+	the console with warning messages. *******************************************************************(LINES COMMENTED OUT IN
 	CASE THEY MAY CAUSE A PROBLEM ON SOME PLATFORMS */
-	/* if (shaderProgram.vertexNormalAttribute == -1) {
+	if (shaderProgram.vertexNormalAttribute == -1) {
 		shaderProgram.vertexNormalAttribute = 2;
-	} */	
+	}
 	gl.enableVertexAttribArray(shaderProgram.vertexNormalAttribute);
 
 	shaderProgram.pMatrixUniform = gl.getUniformLocation(shaderProgram, "uPMatrix");
 	shaderProgram.mvMatrixUniform = gl.getUniformLocation(shaderProgram, "uMVMatrix");
 	shaderProgram.nMatrixUniform = gl.getUniformLocation(shaderProgram, "uNMatrix");
+	
+	// Establish shader properties for the Earth and Moon position for calculating shadows
+	shaderProgram.earthPositionUniform = gl.getUniformLocation(shaderProgram, "earthPosition");
+	shaderProgram.moonPositionUniform = gl.getUniformLocation(shaderProgram, "moonPosition");
+	
 	// The following line also causes the console to fill with
 	// errors in Windows 10
 	//shaderProgram.nMatrixUniform = gl.getUniformLocation(shaderProgram, "uSampler");
@@ -110,7 +108,7 @@ function initTextures() {
 		bindTextures(earthTexture)
 	}
 	earthTexture.image.src = "earth.jpg";
-	
+
 	// Load the tablecloth texture for the table
 	tableTexture = gl.createTexture();
 	tableTexture.image = new Image();
@@ -118,7 +116,7 @@ function initTextures() {
 		bindTextures(tableTexture)
 	}
 	tableTexture.image.src = "tablecloth.jpg";
-	
+
 }
 
 function bindTextures(texture) {
@@ -133,9 +131,11 @@ function bindTextures(texture) {
 }
 
 var mvMatrix = mat4.create();
-var mvMatrixStack = [];
+//var mvMatrixStack = [];
 var pMatrix = mat4.create();
+var nMatrix = mat4.create();
 
+/* These functions appear to do nothing useful in the current implementation of this code
 function mvPushMatrix() {
 	var copy = mat4.create();
 	mat4.set(mvMatrix, copy);
@@ -148,15 +148,31 @@ function mvPopMatrix() {
 	}
 	mvMatrix = mvMatrixStack.pop();
 }
+*/
 
 function setMatrixUniforms() {
 	gl.uniformMatrix4fv(shaderProgram.pMatrixUniform, false, pMatrix);
 	gl.uniformMatrix4fv(shaderProgram.mvMatrixUniform, false, mvMatrix);
 
-	var normalMatrix = mat3.create();
+	/* 
+	I had trouble with this calculation causing my lights to move over 
+	the objects in crazy patterns for reasons I couldn't figure out, 
+	so I opted to abandon this approach and simply rotate the normal vector 
+	matrices in the drawScene() function for each rotating object. Since the 
+	objects are not being scaled, this method should cause no problems.
+	// Rotate normals based on object rotation correcting for situations where an
+	// object is scaled in one or two of its three dimensions only.
+	var normalMatrix = mat4.create();
+	normalMatrix = mvMatrix;
 	mat4.toInverseMat3(mvMatrix, normalMatrix);
-	mat3.transpose(normalMatrix);
-	gl.uniformMatrix3fv(shaderProgram.nMatrixUniform, false, normalMatrix);
+	mat4.transpose(normalMatrix);
+	gl.uniformMatrix4fv(shaderProgram.nMatrixUniform, false, normalMatrix);
+	*/
+	gl.uniformMatrix4fv(shaderProgram.nMatrixUniform, false, nMatrix);
+	
+	// Send Earth and Moon Coordinates to the Shaders
+	gl.uniform3fv(shaderProgram.earthPositionUniform, earthPosition);
+	gl.uniform3fv(shaderProgram.moonPositionUniform, moonPosition);
 }
 
 function degToRad(degrees) {
@@ -181,7 +197,7 @@ var tableVertexIndexBuffer;
 function initBuffers() {
 	// Table initialisation
 	var size = 1;
-	var half = size / 2; 
+	var half = size / 2;
 	var z = 0;
 
 	var vertexPositionData = [-8, -8, 0, 8, -8, 0, -8, 8, 0, 8, 8, 0];
@@ -205,7 +221,7 @@ function initBuffers() {
 	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexPositionData), gl.STATIC_DRAW);
 	tableVertexPositionBuffer.itemSize = 3;
 	tableVertexPositionBuffer.numItems = vertexPositionData.length / 3;
-	
+
 	// Earth initialisation
 	var latitude = 30;
 	var longitude = 30;
@@ -226,7 +242,6 @@ function initBuffers() {
 
 			var x = cosPhi * sinTheta;
 			var y = cosTheta;
-			
 			var z = sinPhi * sinTheta;
 			var u = 1 - (longNumber / longitude);
 			var v = 1 - (latNumber / latitude);
@@ -359,10 +374,12 @@ var moonAngle = 180;
 var earthAngle = 0;
 var earthX = -3.0;
 var earthY = -0.6;
-var earthZ = 0;
-var moonX = 2;
-var moonY = 5;
-var moonZ = 0;
+var earthZ = 4.0;
+var moonX = 2.0;
+var moonY = 5.0;
+var moonZ = 1.0;
+var earthPosition = vec3.create(earthX, earthY, earthZ);
+var moonPosition = vec3.create(moonX, moonY, moonZ);
 
 function drawScene() {
 	gl.viewport(0, 0, canvas.clientWidth, canvas.clientHeight);
@@ -370,13 +387,18 @@ function drawScene() {
 
 	mat4.perspective(45, canvas.clientWidth / canvas.clientHeight, 0.1, 100.0, pMatrix);
 	
+	// Calculate the position of the Earth and Moon
+	earthPosition = [earthX - 17, earthY, earthZ + zoom];
+	moonPosition = [moonX, moonY - 15, moonZ + zoom];
+	
 	// Draw the table
 	mat4.identity(mvMatrix);
+	mat4.identity(nMatrix);
 	// Translate the modelview matrix to the position of
 	// the zoom
 	mat4.translate(mvMatrix, [0, 0, zoom]);
-	
-	mvPushMatrix();
+
+	//mvPushMatrix();
 	//  Load shaders and initialize attribute buffers
     gl.activeTexture(gl.TEXTURE0);
 	gl.bindTexture(gl.TEXTURE_2D, tableTexture);
@@ -393,17 +415,20 @@ function drawScene() {
 
 	setMatrixUniforms();
 	gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
-	mvPopMatrix();
+	//mvPopMatrix();
 
 	// Draw the moon
+	//mvPushMatrix();
 	mat4.identity(mvMatrix);
+	mat4.identity(nMatrix);
+	
 	// Translate the modelview matrix to the position of
 	// the zoom and the current position of the moon in 3d space
-	mat4.translate(mvMatrix, [moonX, moonY - 15, moonZ + zoom]);
+	mat4.translate(mvMatrix, moonPosition);
 
-	mvPushMatrix();
 	// Changed the moon so it rotates faster and around the x-axis
 	mat4.rotate(mvMatrix, degToRad(moonAngle), [-1, 0, 0]);
+	mat4.rotate(nMatrix, degToRad(moonAngle), [-1, 0, 0]);
 	gl.activeTexture(gl.TEXTURE0);
 	gl.bindTexture(gl.TEXTURE_2D, moonTexture);
 	gl.uniform1i(shaderProgram.samplerUniform, 0);
@@ -420,16 +445,18 @@ function drawScene() {
 	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, moonVertexIndexBuffer);
 	setMatrixUniforms();
 	gl.drawElements(gl.TRIANGLES, moonVertexIndexBuffer.numItems, gl.UNSIGNED_SHORT, 0);
-	mvPopMatrix();
+	//mvPopMatrix();
 
 	// Draw the earth
 	mat4.identity(mvMatrix);
+	mat4.identity(nMatrix);
 	// Translate the modelview matrix to the position of
 	// the zoom and the current position of the earth in 3d space
-	mat4.translate(mvMatrix, [earthX - 17, earthY, earthZ + zoom])
-	
-	mvPushMatrix();
+	mat4.translate(mvMatrix, earthPosition)
+
+	//mvPushMatrix();
 	mat4.rotate(mvMatrix, degToRad(earthAngle), [0, 1, 0]);
+	mat4.rotate(nMatrix, degToRad(earthAngle), [0, 1, 0]);
 	gl.bindBuffer(gl.ARRAY_BUFFER, earthVertexPositionBuffer);
 	gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, earthVertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
 
@@ -446,7 +473,7 @@ function drawScene() {
 	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, earthVertexIndexBuffer);
 	setMatrixUniforms();
 	gl.drawElements(gl.TRIANGLES, earthVertexIndexBuffer.numItems, gl.UNSIGNED_SHORT, 0);
-	mvPopMatrix();
+	//mvPopMatrix();
 }
 
 var lastTime = 0;
@@ -454,13 +481,13 @@ var moonDeltaAngle = 0.2;
 var moonDelta = Math.PI * 2 * moonDeltaAngle / 360;
 var earthDeltaAngle = 0.05;
 var earthDelta = Math.PI * 8 * earthDeltaAngle / 360;
-/* 
-The following variable are for the zoom. The motion is not 
-actually a true zoom. It is actually a 'dolly' or 'trucking' 
-motion in filmography language, but these terms are less 
-familiar to most people than the word 'zoom', which is 
-actually what happens when the focal-length of a lense 
-changes. 
+/*
+The following variable are for the zoom. The motion is not
+actually a true zoom. It is actually a 'dolly' or 'trucking'
+motion in filmography language, but these terms are less
+familiar to most people than the word 'zoom', which is
+actually what happens when the focal-length of a lense
+changes.
 */
 var zoom = -19;        // Current zoom distance
 var zoomTarget = -19;  // Where the zoom will end
@@ -477,10 +504,10 @@ function render() {
 		// Calculate x position of the moon
 		earthAngle += earthDeltaAngle * elapsed;
 		earthX = (earthX + earthDelta * elapsed) % 35;
-		
-		/* 
-		Calculate z for when the earth lands, rolls and 
-		falls relative to the table. It is 'thrown' from 
+
+		/*
+		Calculate z for when the earth lands, rolls and
+		falls relative to the table. It is 'thrown' from
 		below the table on the left and goes above it
 		before falling onto it. As it goes over the right
 		edge of the table it falls below it.
@@ -491,18 +518,20 @@ function render() {
 		if (earthX < 9) {
 			earthZ = 16 - Math.pow((earthX - 7.1) / earthDelta  * 0.007, 2);
 		} else {
-			earthZ = 0;		}
+			earthZ = 0;
+		}
+		earthZ = earthZ + 4.0; // Adjust because I discovered that it was buried in the table when I put in the shadows.
 		
 		// Calculate y position of the moon
 		moonAngle += moonDeltaAngle * elapsed;
 		moonY = (moonY + moonDelta * elapsed) % 35;
-		
-		/* 
-		Calculate z for when the moon lands, rolls and 
-		falls relative to the table. It is 'thrown' from 
-		below the table at the bottom of the screen and 
-		goes above the table before falling onto it. 
-		As it goes over the top edge of the table it falls 
+
+		/*
+		Calculate z for when the moon lands, rolls and
+		falls relative to the table. It is 'thrown' from
+		below the table at the bottom of the screen and
+		goes above the table before falling onto it.
+		As it goes over the top edge of the table it falls
 		below it.
 		*/
 		if (moonY > 24) {
@@ -511,31 +540,32 @@ function render() {
 		if (moonY < 9) {
 			moonZ = 16 - Math.pow((moonY - 7) / moonDelta  * 0.007, 2);
 		} else {
-			moonZ = 0;
+			moonZ = 0; 
 		}
-		
+		moonZ = moonZ + 1.0; // Adjust because I discovered that it was buried in the table when I put in the shadows. 
+
 		// Eased slow zoom to target zoom distance
-		/* 
-		The motion used here is not actually a true zoom. 
-		It is actually a 'dolly' or 'trucking' motion in 
-		filmography language, but these terms are less 
-		familiar to most people than the word 'zoom', which 
-		is actually what happens when the focal-length of a 
+		/*
+		The motion used here is not actually a true zoom.
+		It is actually a 'dolly' or 'trucking' motion in
+		filmography language, but these terms are less
+		familiar to most people than the word 'zoom', which
+		is actually what happens when the focal-length of a
 		lense changes.
 
         I added a Gaussian function to ease the motion so
 		it looks a little more natural. That is to say, the
         zoom starts slow, goes a little faster in the middle
-        and slows again before finishing.		
+        and slows again before finishing.
 		*/
 		var threshhold = 0.2;	// A non-zero value prevents bouncing at the end of the zoom run
 		var peakWidth = 0.45;	// The lower the number, the sharper the change in the acceleration between the ends and the middle of the motion
-        var peakHeight = 0.4;	// The higher the number, the faster the zoom completes			
+        var peakHeight = 0.4;	// The higher the number, the faster the zoom completes
 		var peakCentre = 0.5;	// 0.5 means it peaks in the centre of the motion
-		
+
 		//elapsedAverage = (elapsedAverage + elapsed) / 2;
 		//elapsedMax = Math.max(elapsedMax, elapsed);
-		if (zoom != zoomTarget) 
+		if (zoom != zoomTarget)
 		{
 			var zoomDifference = zoomTarget - zoom;
 			// alert("Average Elapsed = " + elapsedAverage + "; Maximum Elapsed = " + elapsedMax);
@@ -546,7 +576,7 @@ function render() {
 			var easingFunction = Math.pow(Math.E, -Math.pow((zoomProgress - peakCentre) / peakWidth, 2)) * peakHeight;
 			if (zoomDifference < -threshhold) {
 				zoom = zoom - easingFunction;
-			} else 
+			} else
 			if (zoomDifference > threshhold) {
 				zoom = zoom + easingFunction;
 			} else {
